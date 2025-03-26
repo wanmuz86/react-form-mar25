370cda8e5d4c958a22d58c035f08ea6ce89d17dc
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-26 13:43:39 +0700

    Add Select

--- src/components/ControlledForm.jsx
+++ src/components/ControlledForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const ControlledForm = () => {
 const [name,setName] = useState('')
+const [gender,setGender] = useState('male')
   return (
     <div>
         <h2>Controller Form</h2>
@@ -9,6 +10,16 @@ const [name,setName] = useState('')
         <label htmlFor="name">Name:</label>
         <input type="text" name='name' id='name' 
         value={name} onChange={(e)=> setName(e.target.value)}/>
+        </div>
+        <div>
+            <label htmlFor="gender">Gende</label>
+            <select name="gender" id="gender" value={gender}
+            onChange={(e)=> setGender(e.target.value)}>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="notstate">Prefer not to state</option>
+            </select>
+            <p>User has selected {gender}</p>
         </div>
     </div>
   )
