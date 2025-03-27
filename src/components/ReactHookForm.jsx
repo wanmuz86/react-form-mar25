5366d319744ae1cfdcef1540079c10998454c307
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-27 08:37:31 +0700

    add watcher

--- src/components/ReactHookForm.jsx
+++ src/components/ReactHookForm.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import  { useForm }  from  "react-hook-form";
 
 
 const ReactHookForm = () => {
     // 1 -> Register a form , 2 -> Handle the form submission, 3 -> Display the form errors
-    const {register, handleSubmit, formState:{errors}} = useForm()
+    // Add watch to the useForm hook to watch a form fields
+    const {register,watch, handleSubmit, formState:{errors}} = useForm()
+    const [warning,setWarning] = useState('')
+
 
     const onSubmit = (data) => {
         console.log(data)
     }
+
+
+    const watchMessage = watch('message')
+
+    useEffect(() => {
+
+        const subscription = watch((data) => {
+       
+            if (data.message?.toLowerCase().includes('hack') || data.message?.toLowerCase().includes('profanity')
+            || data.message?.toLowerCase().includes('porn')){
+                setWarning('Profanity word detected')
+        }
+    }
+        )
+        return () => subscription.unsubscribe();
+    
+    },[watchMessage])
+
+    // useEffect(() => {
+    //     const subscription = watch((value, { name }) => {
+    //         if (name === 'message') {
+    //             console.log(value);
+    //         }
+    //     });
+    
+    //     return () => subscription.unsubscribe();
+    // }, [watch]);
+
   return (
     <div>
         <h2>Example with React Form</h2>
@@ -39,6 +70,7 @@ const ReactHookForm = () => {
             <div>
             <label htmlFor="message">Message</label>
             <textarea {...register("message", {required: true})} />
+            {warning && <p style={{color:'red'}}>{warning}</p>}
             {errors.message && <p style={{color:'red'}}>Message is required</p>}
             </div>
             
